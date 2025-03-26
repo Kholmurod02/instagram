79c8d56976cc4723cd0a4481f70738ef44189d11
CodeByMaks <maks667741@gmail.com>
2025-03-27 00:45:39 +0500

    add some features

--- src/widgets/about-me-homepage.tsx
+++ src/widgets/about-me-homepage.tsx
@@ -33,7 +33,7 @@ const AboutMe = () => {
 		<div className='py-3 text-white'>
 			<div className='flex justify-between items-center'>
 				<div className='flex gap-3 items-center'>
-					<div className='w-12 h-12 rounded-full p-[1px] border-2 border-transparent bg-gradient-to-bl to-yellow-500 via-red-500 from-pink-500'>
+					<div className='w-12 h-12 rounded-full p-[1px] border-2 cursor-pointer border-transparent bg-gradient-to-bl to-yellow-500 via-red-500 from-pink-500'>
 						<div className='w-full h-full rounded-full bg-white p-[2px]'>
 							<img
 								src={`https://instagram-api.softclub.tj/images/${profile.image}`}
