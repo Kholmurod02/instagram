1d13a51d535b62daee421e27f2e525d52d4be812
CodeByMaks <maks667741@gmail.com>
2025-03-27 13:51:33 +0500

    fix text access_token

--- src/entities/story-homepage/story-homepage.tsx
+++ src/entities/story-homepage/story-homepage.tsx
@@ -1,17 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiJkMmEyYzlhYi1lMmM0LTQwNmUtOWVmOS03NGNhZjRlZmFkY2YiLCJuYW1lIjoibWFrczAwNSIsImVtYWlsIjoibWFrc0BnbWFpbC5jb20iLCJzdWIiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJVc2VyIiwiZXhwIjoxNzQzMDUzMjgwLCJpc3MiOiJpbnN0YWdyYW0tZ3JvdXAiLCJhdWQiOiJpbnN0YWdyYW0tYXBpIn0.gLrOySeblVgxCkRF5tv5ITW9mIPTGWNbnNkoGfKHAEI"
-
 export const historyApi = createApi({
 	reducerPath: 'historyApi',
 	baseQuery: fetchBaseQuery({
 		baseUrl: 'https://instagram-api.softclub.tj/Story/',
 		prepareHeaders: (headers) => {
-			const token = localStorage.getItem('token');
-			console.log(token);
+			const access_token = localStorage.getItem('access_token');
+			console.log(access_token);
 			
-			if (token) {
-				headers.set("Authorization", `Bearer ${token}`);
+			if (access_token) {
+				headers.set("Authorization", `Bearer ${access_token}`);
 			 }
 			
 			return headers;
