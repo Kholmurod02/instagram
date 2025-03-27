f7810ac4b8b34eb3c714d86ec229f72811123f37
Kholmurod <khairulloev.kh@gmail.com>
2025-03-27 13:27:44 +0500

    getChats&getchatbyid

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,14 +1,16 @@
+import { chatApi } from '@/entities/chats/chat-api'
 import { postApi } from '@/entities/post/postApi'
 import { reelsApi } from '@/entities/reels/reels'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   reducer: {
-    [postApi.reducerPath]: postApi.reducer, 
+    [postApi.reducerPath]: postApi.reducer,
     [reelsApi.reducerPath]: reelsApi.reducer,
+    [chatApi.reducerPath]: chatApi.reducer
   },
-  middleware: (getDefaultMiddleware) => 
-    getDefaultMiddleware().concat(postApi.middleware, reelsApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(postApi.middleware, reelsApi.middleware, chatApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
