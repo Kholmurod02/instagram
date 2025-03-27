718039f409d851c062217292cab2011146059fd6
TblScripte <tegnievbilol@gmail.com>
2025-03-27 16:13:00 +0500

    fix

--- src/entities/reels/reels.ts
+++ src/entities/reels/reels.ts
@@ -1,31 +1,43 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiIxM2NiMzllMi02YjcxLTQwMzItODhmYi1lMTM3N2E5YmJjNzAiLCJuYW1lIjoic3RyaW5nMTEyIiwiZW1haWwiOiJzdHJpbmcxMTIiLCJzdWIiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJVc2VyIiwiZXhwIjoxNzQzMDU0Nzc5LCJpc3MiOiJpbnN0YWdyYW0tZ3JvdXAiLCJhdWQiOiJpbnN0YWdyYW0tYXBpIn0.30oBQ9y-Db81o3Zcwg3YvvChkLPCa2qIwjL2LNlD7-Q";
-
 export const reelsApi = createApi({
-  reducerPath: 'reelsApi',
+  reducerPath: "reelsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://instagram-api.softclub.tj/Post/',
+    baseUrl: "https://instagram-api.softclub.tj/",
     prepareHeaders: (headers) => {
-      const token = localStorage.getItem('authToken');
-		console.log(token);
+      const token = localStorage.getItem("access_token");
+      console.log("🔑Токен:", token);
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
+        headers.set("Authorization", `Bearer ${token}`);
       }
       return headers;
     },
   }),
   endpoints: (builder) => ({
-	getReels: builder.query({
-	  query: () => 'get-reels',
-	}),
-	likeReel: builder.mutation({
-	  query: (reelId) => ({
-      url: `like-post?postId=${reelId}`,
-      method: 'POST',
+    getReels: builder.query({
+      query: () => "Post/get-reels?PageSize=10000",
     }),
-	}),
- }),
-})
 
-export const { useGetReelsQuery,useLikeReelMutation } = reelsApi
+    following: builder.mutation({
+      query: (userId) => ({
+        url: `FollowingRelationShip/add-following-relation-ship?followingUserId=${userId}`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+    }),
+
+    likeReel: builder.mutation({
+      query: (reelId) => ({
+        url: `Post/like-post?postId=${reelId}`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useGetReelsQuery, useLikeReelMutation, useFollowingMutation } = reelsApi;
--- src/pages/(protected)/reels/page.tsx
+++ src/pages/(protected)/reels/page.tsx
@@ -1,125 +1,216 @@
 import * as React from 'react'
 import { Card, CardContent } from '@/shared/ui/card'
 import { Carousel, CarouselContent, CarouselItem } from '@/shared/ui/carousel'
-import { Bookmark, Ellipsis, Heart, MessageCircle, Send } from 'lucide-react'
-import { useGetReelsQuery, useLikeReelMutation } from '@/entities/reels/reels'
+import {
+	Bookmark,
+	Ellipsis,
+	Heart,
+	MessageCircle,
+	Send,
+} from 'lucide-react'
+import {
+	useFollowingMutation,
+	useGetReelsQuery,
+	useLikeReelMutation,
+} from '@/entities/reels/reels'
 
 export default function ReelsPage() {
-  const [activeVideo, setActiveVideo] = React.useState<number | null>(null)
-  const videoRefs = React.useRef<HTMLVideoElement[]>([])
-  const { data: reels, error, isLoading } = useGetReelsQuery("")
-  const [pausedVideo, setPausedVideo] = React.useState<number | null>(null)
-  const [liked, setLiked] = React.useState<{ [key: string]: boolean }>({})
+	const [activeVideo, setActiveVideo] = React.useState<number | null>(null)
+	const videoRefs = React.useRef<HTMLVideoElement[]>([])
+	const { data: reels, error, isLoading } = useGetReelsQuery('')
+	const [followUser] = useFollowingMutation()
+	const [pausedVideo, setPausedVideo] = React.useState<number | null>(null)
+	const [liked, setLiked] = React.useState<{ [key: string]: boolean }>({})
+	const [messangeDialog, setMessangeDialog] = React.useState<boolean>(false)
+	const [openedCommentDialog, setOpenedCommentDialog] = React.useState<
+		null | boolean
+	>(false)
+	const [likeReel] = useLikeReelMutation()
 
-  const [likeReel] = useLikeReelMutation(); 
+	if (isLoading) return <div className='w-[600px] ml-[50%] m-auto bg-green flex justify-center h-[60vh] items-center absolute top-0 right-[470px]'>Loading...</div>
+	const handlePlayPause = (index: number) => {
+		const video = videoRefs.current[index]
+		if (!video) return 
 
-  if (isLoading) return <h1>Loading</h1>
-  
-  const handlePlayPause = (index: number) => {
-    const video = videoRefs.current[index]
-    if (!video) return
+		if (video.paused) {
+			videoRefs.current.forEach((vid, i) => {
+				if (vid && i !== index) vid.pause()
+			})
+			video.play()
+			setActiveVideo(index)
+			setPausedVideo(null)
+		} else {
+			video.pause()
+			setPausedVideo(index)
+			setActiveVideo(null)
+		}
+	}
 
-    if (video.paused) {
-      videoRefs.current.forEach((vid, i) => {
-        if (vid && i !== index) vid.pause()
-      })
-      video.play()
-      setActiveVideo(index)
-      setPausedVideo(null)
-    } else {
-      video.pause()
-      setPausedVideo(index)
-      setActiveVideo(null)
-    }
-  }
+	const handleLikeClick = (reelId: string) => {
+		setLiked(prev => ({ ...prev, [reelId]: !prev[reelId] }))
+		likeReel(reelId)
+	}
+	console.log(messangeDialog)
 
-  const handleLikeClick = (reelId: string) => {
-    setLiked((prev) => ({ ...prev, [reelId]: !prev[reelId] }))
-    likeReel(reelId);
-  }
-
-  return (
-    <div className='w-[600px] ml-[50%] m-auto bg-green flex justify-center h-[60vh] items-center'>
-      <Carousel opts={{ align: 'start' }} orientation='vertical' className='ml-[10px] w-[600px] m-auto h-[60vh]'>
-        <CarouselContent className='mt-1 w-[620px] h-[99vh]'>
-          {reels?.data?.map((reel: any, index: any) => (
-            <CarouselItem key={index} className='md:basis-1/2 '>
-              <div className='p-1 w-[560px] m-auto flex justify-center'>
-                <Card className='text-center bg-transparent border-none w-[600px] h-[90vh] mt-[10px] mb-[10px] flex justify-center items-center'>
-                  <CardContent className='relative w-full h-full'>
-                    <div className='w-full h-full flex justify-center'>
-                      <div className='flex'>
-                        <div className='w-full h-full flex justify-center'>
-                          <div className='relative'>
-                            <div className='absolute inset-0 w-[450px] h-[600px] bg-black opacity-40 z-1 rounded-lg blur-lg'></div>
-                            <video
-                              ref={el => {
-                                if (el) videoRefs.current[index] = el;
-                              }}
-                              autoPlay loop playsInline 
-                            
-                              className='relative w-[500px] h-[700px] cursor-pointer z-1 transition-all duration-300 rounded-lg'
-                              onClick={() => handlePlayPause(index)}
-                            >
-                              <source
-                                src={`https://instagram-api.softclub.tj/images/${reel.images}`}
-                                type='video/mp4'
-                              />
-                              Your browser does not support the video tag.
-                            </video>
-                            {pausedVideo === index && (
-                              <div className='absolute inset-0 flex items-center justify-center z-0 duration-300'>
-                                <button
-                                  onClick={() => handlePlayPause(index)}
-                                  className='rounded-full transition-transform duration-300 '
-                                >
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide w-15 h-15 text-white ml-[23px] bg-black p-[20px] opacity-[0.6]  rounded-full">
-                                    <polygon points="6 3 20 12 6 21 6 3" />
-                                  </svg>
-                                </button>
-                              </div>
-                            )}
-                            <div className='flex items-center space-x-3 text-white absolute bottom-[-10px] left-[70px] z-30'>
-                              <img src={"https://instagram-api.softclub.tj/images/" + reel.userImage} className='rounded-full w-12 h-12 border-2 border-white' alt="User" />
-                                <h1 className='text-lg font-semibold ml-[10px] pr-[20px]'>{reel.userName}</h1>
-                                <button className='text-sm font-medium text-white px-5 py-3 rounded-md bg-blue-500'>Подписаться</button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className='absolute bottom-[-30px] z-10 right-5 flex flex-col items-center space-y-4'>
-                        <div className='flex flex-col items-center'>
-                          <Heart
-                            className={`w-6 h-6 ${reel.postLike ? "text-red-500 fill-amber-700" : "text-white"}`}
-                            onClick={() => handleLikeClick(reel.postId)}
-                          />
-                          <h1>{reel.postLikeCount}</h1>
-                        </div>
-                        <div className='flex flex-col items-center'>
-                          <MessageCircle className='w-6 h-6' />
-                          <h1>{reel.commentCount}</h1>
-                        </div>
-                        <div className='flex flex-col items-center'>
-                          <Send className='w-76h-76' />
-                        </div>
-                        <div className='flex flex-col items-center'>
-                          <Bookmark className='w-76h-76' />
-                        </div>
-                        <div className='flex flex-col items-center'>
-                          <Ellipsis className='w-76h-76' />
-                        </div>
-                        <div className=' bg-violet-600 rounded-full'>
-                          <img src={"https://instagram-api.softclub.tj/images/" + reel.userImage} className='rounded-full w-[40px] h-[42px]' alt="" />
-                        </div>
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
-    </div>
-  )
+	return (
+		<>
+			<div className='w-[600px] ml-[50%] m-auto bg-green flex justify-center h-[60vh] items-center absolute top-0 right-[470px]'>
+				<Carousel
+					opts={{ align: 'start' }}
+					orientation='vertical'
+					className='ml-[10px] w-[600px] m-auto h-[60vh]'
+				>
+					<CarouselContent className='mt-1 w-[650px] h-[90vh]'>
+						{reels?.data?.map((reel: any, index: any) => (
+							<CarouselItem key={index} className='md:basis-1/2 '>
+								<div className='p-1 w-[560px]  m-auto flex justify-center  z-10'>
+									<Card className='text-center bg-transparent  z-10 border-none  w-[600px] h-[90vh] mt-[10px] flex justify-center  items-center'>
+										<CardContent className='relative w-full  z-10 h-full '>
+											<div className='w-full h-full flex justify-center '>
+												<div className='flex z-10 '>
+													<div className='w-full  z-10  h-full flex justify-center'>
+														<div className='relative  z-10'>
+															<div className='absolute  inset-0 w-[450px] h-[600px] bg-black opacity-40 z-1 rounded-lg blur-lg'></div>
+															<video
+																ref={el => {
+																	if (el) videoRefs.current[index] = el
+																}}
+																autoPlay
+																loop
+																playsInline
+																className='relative w-[500px] h-[700px] cursor-pointer z-1 transition-all duration-300 rounded-lg'
+																onClick={() => handlePlayPause(index)}
+															> 
+																<source
+																	src={`https://instagram-api.softclub.tj/images/${reel.images}`}
+																	type='video/mp4'
+																	className='w-[100%] h-[100px]'
+																/>
+																Your browser does not support the video tag.
+															</video>
+															{pausedVideo === index && (
+																<div
+																	className='absolute inset-0 flex items-center justify-center z-20 
+                                  opacity-100 transition-opacity duration-100 ease-in-out'
+																>
+																	<button
+																		onClick={() => handlePlayPause(index)}
+																		className='rounded-full transition-transform duration-300 scale-95 hover:scale-105'
+																	>
+																		<svg
+																			xmlns='http://www.w3.org/2000/svg'
+																			width='50'
+																			height='50'
+																			viewBox='0 0 24 24'
+																			fill='white'
+																			stroke='currentColor'
+																			strokeWidth='2'
+																			strokeLinecap='round'
+																			strokeLinejoin='round'
+																			className='lucide w-20 h-20 text-white opacity-80 p-7 rounded-full bg-black'
+																		>
+																			<polygon points='6 3 20 12 6 21 6 3' />
+																		</svg>
+																	</button>
+																</div>
+															)}
+															<div className='flex items-center space-x-3 text-white absolute bottom-[20px] left-[70px] z-30'>
+																<img
+																	src={
+																		'https://instagram-api.softclub.tj/images/' +
+																		reel.userImage
+																	}
+																	className='rounded-full w-12 h-12 border-2 border-white'
+																	alt='User'
+																/>
+																<h1 className='text-lg font-semibold ml-[10px] pr-[20px]'>
+																	{reel.userName}
+																</h1>
+																<button
+																	onClick={() => followUser(reel.userId)}
+																	className={`text-sm font-medium text-white px-5 py-3 rounded-md bg-transparent shadow-md border-gray-400 border`}
+																>
+																	{reel.isSubscriber
+																		? 'Вы Подписани'
+																		: 'Подписаться'}
+																</button>
+															</div>
+														</div>
+													</div>
+												</div>
+												<div className='absolute bottom-[0px] z-10 right-5 flex flex-col items-center space-y-4'>
+													<div className='flex flex-col items-center'>
+														<Heart
+															className={`w-6 h-6 ${
+																reel.postLike
+																	? 'text-red-500 fill-amber-700 '
+																	: 'text-white'
+															}`}
+															onClick={() => handleLikeClick(reel.postId)}
+														/>
+														<h1>{reel.postLikeCount}</h1>
+													</div>
+													<div className='flex flex-col items-center'>
+														<MessageCircle
+															className='w-6 h-6'
+															onClick={() => setOpenedCommentDialog(index)}
+														/>
+														<h1>{reel.commentCount}</h1>
+													</div>
+													<div className='flex flex-col items-center'>
+														<Send className='w-76h-76' />
+													</div>
+													<div className='flex flex-col items-center'>
+														<Bookmark className='w-76h-76' />
+													</div>
+													<div className='flex flex-col items-center'>
+														<Ellipsis className='w-76h-76' />
+													</div>
+													<div className=' bg-violet-600 rounded-full'>
+														<img
+															src={
+																'https://instagram-api.softclub.tj/images/' +
+																reel.userImage
+															}
+															className='rounded-full w-[40px] h-[42px]'
+															alt=''
+														/>
+													</div>
+												</div>
+											</div>
+										</CardContent>
+										<div className='flex flex-col items-center relative z-20 left-[400px]'>
+											{openedCommentDialog === index && (
+												<div className='absolute bottom-12 right-0 w-[200px] bg-white text-black p-3 rounded shadow-lg z-50'>
+													<h2 className='font-semibold mb-2'>
+														Напишите комментарий
+													</h2>
+													<textarea
+														className='w-full border p-1 rounded text-sm'
+														rows={2}
+														placeholder='Ваш комментарий...'
+													></textarea>
+													<div className='flex justify-end mt-2 space-x-2'>
+														<button
+															onClick={() => setOpenedCommentDialog(null)}
+															className='text-sm px-2 py-1 bg-gray-300 rounded'
+														>
+															Отмена
+														</button>
+														<button className='text-sm px-2 py-1 bg-blue-500 text-white rounded'>
+															Отправить
+														</button>
+													</div>
+												</div>
+											)}
+										</div>
+									</Card>
+								</div>
+							</CarouselItem>
+						))}
+					</CarouselContent>
+				</Carousel>
+			</div>
+		</>
+	)
 }
