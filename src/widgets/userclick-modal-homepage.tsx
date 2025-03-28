50af662799b34f5c223b6fca2aa5aceb3359980c
CodeByMaks <maks667741@gmail.com>
2025-03-28 12:33:41 +0500

    fix modal homepage

--- src/widgets/userclick-modal-homepage.tsx
+++ src/widgets/userclick-modal-homepage.tsx
@@ -1,40 +1,80 @@
-import { useState } from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/shared/ui/dialog";
-import { Button } from "@/shared/ui/button";
 import { useGetUsersHomepageQuery } from '@/entities/users-homepage/users-homepage'
+import { Button } from '@/shared/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/shared/ui/dialog'
+import { useState } from 'react'
 
 const UserclickModalHomepage = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
   const { data, error, isLoading } = useGetUsersHomepageQuery(undefined)
-  if(error) return <div>Error loading users</div>
-  if(!data || data.length === 0) return <div>No users found</div>
-  if(isLoading) return <div>Loading...</div>
 
+  if (error)
+    return (
+      <div className='flex justify-center items-center h-full text-white font-semibold'>
+        Error loading users
+      </div>
+    )
+  if (!data || data.length === 0) return <div>No users found</div>
+  if (isLoading)
+    return (
+      <div className='flex justify-center items-center h-full text-white font-semibold'>
+        Loading...
+      </div>
+    )
 
   return (
     <>
       <div className='flex justify-between'>
         <h2 className='text-gray-300'>Recomendate for you</h2>
-        <p className='hover:text-gray-400 cursor-pointer' onClick={() => setOpen(true)}>All</p>
+        <p
+          className='hover:text-gray-400 cursor-pointer'
+          onClick={() => setOpen(true)}
+        >
+          All
+        </p>
       </div>
-      
+
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent>
+        <DialogContent className="max-h-[90vh] overflow-hidden"> 
           <DialogHeader>
-            <DialogTitle>Заголовок модального окна</DialogTitle>
+            <DialogTitle>Recomendations</DialogTitle>
           </DialogHeader>
-          {data && (
-            <ul>
-              {data.data.map((user) => (
-                <li key={user.id} className='border-b py-2'>{user.userName}</li>
-              ))}
-            </ul>
-          )}
-          <Button onClick={() => setOpen(false)}>Закрыть</Button>
+          <div className="overflow-y-auto max-h-[calc(90vh-100px)]" 	style={{
+								scrollbarWidth: 'none',
+								msOverflowStyle: 'none',
+							}}>
+            {data && (
+              <ul>
+                {data.data.map(user => (
+                  <li key={user.id} className='py-3'>
+                    <div className='flex items-center gap-2'>
+                      <div className='w-12 h-12 rounded-full p-[1px] border-2 border-transparent bg-gradient-to-bl to-yellow-500 via-red-500 from-pink-500'>
+                        <div className='w-full h-full rounded-full bg-white p-[2px]'>
+                          <img
+                            className='rounded-full w-full h-full object-cover'
+                            src={`https://instagram-api.softclub.tj/images/${user.avatar}`}
+                            alt=''
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <h3>{user.userName}</h3>
+                        <p className='text-[13px] text-gray-300'>{user.fullName}</p>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </DialogContent>
       </Dialog>
     </>
-  );
-};
+  )
+}
 
-export default UserclickModalHomepage;
+export default UserclickModalHomepage
