762b7eb697b2d1b10e501e65c28f4da316693bcd
CodeByMaks <maks667741@gmail.com>
2025-03-29 10:02:36 +0500

    fix homepage posts

--- src/widgets/about-me-homepage.tsx
+++ src/widgets/about-me-homepage.tsx
@@ -31,7 +31,7 @@ const AboutMe = () => {
 		<div className='py-3 text-white'>
 			<div className='flex justify-between items-center'>
 				<div className='flex gap-3 items-center'>
-					<div onClick={handleClick} className='w-12 h-12 rounded-full p-[1px] border-2 cursor-pointer border-transparent bg-gradient-to-bl to-yellow-500 via-red-500 from-pink-500'>
+					<div className='w-12 h-12 rounded-full p-[1px] border-2 cursor-pointer border-transparent bg-gradient-to-bl to-yellow-500 via-red-500 from-pink-500'>
 						<div className='w-full h-full rounded-full bg-white p-[2px]'>
 							<img
 								src={`https://instagram-api.softclub.tj/images/${profile.image}`}
@@ -40,7 +40,7 @@ const AboutMe = () => {
 							/>
 						</div>
 					</div>
-					<div>
+					<div onClick={handleClick} className='cursor-pointer'>
 						<h2>{profile.userName || 'Unknown User'}</h2>
 						<p className='text-gray-300 text-sm'>{data.data?.firstName}</p>
 					</div>
