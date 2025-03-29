6f2dfc4fa4fc87e22d08e2917b2ac0c7458127d3
CodeByMaks <maks667741@gmail.com>
2025-03-29 11:47:20 +0500

    .

--- src/features/history/history.tsx
+++ src/features/history/history.tsx
@@ -8,7 +8,7 @@ const History = ({story}) => {
 		<div className='flex flex-col justify-center items-center'> 
 		<div onClick={()=> setOpen(true)} className='shrink-0  w-16 h-16 rounded-full p-[1px] border-2 text-white border-transparent bg-gradient-to-bl to-yellow-500 via-red-500 from-pink-500'>
 			<div className='w-full h-full rounded-full bg-white p-[2px]'>
-				<img className='rounded-full w-full h-full object-cover' src={`https://instagram-api.softclub.tj/images/${story.userImage}`} alt={story.userName} />
+				<img className='rounded-full w-full h-full object-cover' src={`https://instagram-api.softclub.tj/images/${story.userImage}`} alt={story.userName} /> 
 			</div>
 		</div>
 			<p className='text-[12px] text-center py-1'>{story.userName}</p>
