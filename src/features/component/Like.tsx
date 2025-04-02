f301577ff283c427f317625c8772391bb698719e
CodeByMaks <maks667741@gmail.com>
2025-04-02 22:13:45 +0500

    fix like

--- src/features/component/Like.tsx
+++ src/features/component/Like.tsx
@@ -6,10 +6,16 @@ interface LikeProps {
   postId: string
   initialLiked?: boolean
   initialLikes?: number
-  size?: number; 
+  size?: number
+  onLikeChange?: (isLiked: boolean, newLikesCount: number) => void
 }
 
-const Like: React.FC<LikeProps> = ({ postId, initialLiked = false, initialLikes = 0 }) => {
+const Like: React.FC<LikeProps> = ({ 
+  postId, 
+  initialLiked = false, 
+  initialLikes = 0,
+  onLikeChange
+}) => {
   const [likePost] = useLikePostMutation()
 
   const storedLikes: Record<string, boolean> =
@@ -28,23 +34,34 @@ const Like: React.FC<LikeProps> = ({ postId, initialLiked = false, initialLikes 
   const handleLike = async () => {
     const newLikedState = !liked
     const newLikesCount = newLikedState ? likes + 1 : likes - 1
+    
     setLiked(newLikedState)
     setLikes(newLikesCount)
+    
     const updatedLikes = { ...storedLikes, [postId]: newLikedState }
     const updatedLikesCount = { ...storedLikesCount, [postId]: newLikesCount }
     localStorage.setItem('likedPosts', JSON.stringify(updatedLikes))
     localStorage.setItem('likesCount', JSON.stringify(updatedLikesCount))
+   
+    if (onLikeChange) {
+      onLikeChange(newLikedState, newLikesCount)
+    }
+
     try {
       await likePost(postId)
     } catch (error) {
       console.error('Error liking post:', error)
-      setLiked(!newLikedState) 
-      setLikes(likes) 
+     
+      setLiked(!newLikedState)
+      setLikes(likes)
+      if (onLikeChange) {
+        onLikeChange(!newLikedState, likes)
+      }
     }
   }
 
   return (
-    <button onClick={handleLike} className="flex items-center gap-1">
+    <button onClick={handleLike} className="flex items-center cursor-po gap-1">
       <Heart
         size={35}
         className={liked ? 'fill-red-500 text-red-500' : 'text-gray-100'}
